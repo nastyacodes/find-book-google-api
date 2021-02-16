3a84ca68bba1d73f840e47a169e32749988a1634
Anastasia Shpit <itshpit@gmail.com>
2021-02-16 13:44:45 +0200

    Add comments to JS file

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const btnSearch = document.querySelector('#btn-search');
 const divResults = document.querySelector('#search-results');
 const inputSearch = document.querySelector('#input-search');
 
+// Search on click on a serch button
 btnSearch.addEventListener('click', fetchAPI);
+// Search on clicking Enter
 inputSearch.addEventListener('keyup', function(e) {
     if(e.keyCode === 13) {
         fetchAPI();
@@ -10,32 +12,38 @@ inputSearch.addEventListener('keyup', function(e) {
 });
 
 function fetchAPI() {
+    // Definitions of phrase to search and API KEY
     const searchPhrase = inputSearch.value ? inputSearch.value : 'javascript';
     const apiKey = 'AIzaSyCLhvQyJ4Zx-TqXHw_EEp4MD1sX6fo5WxQ';
 
+    // Fetch Google Books API
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchPhrase}&maxResults=40&key=${apiKey}`)
-    .then(response => response.json())
-    .then(data => data.items.reverse())
+    .then(response => response.json()) // receive response in JSON format
+    .then(data => data.items.reverse()) // reverse items in books array
     .then(books => {
-        let resultHtml = ``;
+        let resultHtml = ``; // here we save created html elements
 
         books.forEach(book => {
             const bookInfo = book.volumeInfo;
 
+            // books info displayed on the search page
             const image = bookInfo.imageLinks ? bookInfo.imageLinks.smallThumbnail : '/';
             const title = bookInfo.title;
             const subtitle = bookInfo.subtitle ? bookInfo.subtitle : '';
             const description = bookInfo.description ? cutDescription(bookInfo.description) : 'Description not found...';
             const link = bookInfo.infoLink;
 
+            // add a book card to the html markup
             resultHtml += showBook(image, title, subtitle, description, link);
         });
 
+        // display created html markup
         divResults.innerHTML = `<div class="row mx-auto">${resultHtml}</div>`;
     });
     
 }
 
+// create html markup for one book
 function showBook(image, title, subtitle, description, link) {
     return `
         <div class="card col-xs-6 col-sm-4 col-md-3 col-lg-2" style="width: 18rem;">
@@ -49,6 +57,7 @@ function showBook(image, title, subtitle, description, link) {
         </div>`;
 }
 
+// decrease the size of the long description to 100 symbols + add '...' at the end
 function cutDescription(description) {
     return description.slice(0, 100).concat('', '...');
 }
